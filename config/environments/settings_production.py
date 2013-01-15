d56ac33c4e1178ecf7a3eef36a7c3c81cc715476
Lee Solway <lee@zenn.local>
2013-01-15 10:43:34 +0000

    Name updates
    Renamed common to the common settings_common naming convention.
    Created Environments config folder to contain various server configs

---
+++ config/environments/settings_production.py
@@ -0,0 +1,14 @@
+from settings_common import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE'    : 'django.db.backends.mysql',
+        'NAME'      : '',
+        'USER'      : '',
+        'PASSWORD'  : '',
+        'HOST'      : '127.0.0.1',
+        'PORT'      : '',
+    }
+}
